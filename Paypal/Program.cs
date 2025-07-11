b2ba887e3193792ff3e561659c8009f1c80a34fa
angel5000 <delllancer@gmail.com>
2025-07-10 20:51:49 -0500

    cambio de version de .net

--- Paypal/Program.cs
+++ Paypal/Program.cs
@@ -3,8 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
@@ -20,7 +19,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+  
     app.UseSwagger();
     app.UseSwaggerUI();
 }
